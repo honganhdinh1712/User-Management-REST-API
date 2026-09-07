16f6f36283f421f8659e1dd7787725befc4b23e2
honganhdinh1712 <honganhdinh1712@gmail.com>
2026-09-07 17:25:12 +0700

    POST create an user

--- server.js
+++ server.js
@@ -2,76 +2,131 @@ const http = require("node:http");
 
 const PORT = 3000;
 
+// Dummy data
+const users = [
+    {
+        "id": 1,
+        "name": "A"
+    },
+    {
+        "id": 2,
+        "name": "B"
+    }
+]
 const server = http.createServer((req, res) => {
     const parts = req.url.split("/");
 
-    if (
-        req.method === "GET" &&
-        parts[1] === "users"
-    ) {
-        const users = [
-            {
-                "id": 1,
-                "name": "A"
-            },
-            {
-                "id": 2,
-                "name": "B"
-            }
-        ]
-        const id = Number(parts[2]);
+    // GET METHOD ONLY
+    if (req.method === "GET") {
+        if (
+            parts[1] === "users"
+        ) {
+            const id = Number(parts[2]);
 
-        if (!Number.isInteger(id) || id <= 0) {
-            const user = users.find(user => user.id === id);
-            if (user) {
+            if (Number.isInteger(id) && id >= 0) {
+                const user = users.find(user => user.id === id);
+                if (user) {
+                    res.writeHead(200, {
+                        "Content-Type": "application/json"
+                    });
+                    
+                    res.end(JSON.stringify({
+                        user: user
+                    }))
+                } else {
+                    res.writeHead(404, {
+                        "Content-Type": "application/json"
+                    });
+                    
+                    res.end(JSON.stringify({
+                        message: `Not found user ${id}`
+                    }))
+                }
+            } else if (!Number.isInteger(id) || id <= 0) {
+                res.writeHead(400, {
+                    "Content-Type": "application/json"
+                });
+            
+                res.end(JSON.stringify({
+                    message: "Id must is integer and not a negative number"
+                }))
+            } else {
                 res.writeHead(200, {
                     "Content-Type": "application/json"
                 });
-                
+            
                 res.end(JSON.stringify({
-                    user: user
-                }))
-            } else {
-                res.writeHead(404, {
-                    "Content-Type": "application/json"
-                });
-                
-                res.end(JSON.stringify({
-                    message: `Not found user ${id}`
+                    users: users
                 }))
             }
-        } else {
+        } else if (parts[1] === 'about') {
             res.writeHead(200, {
                 "Content-Type": "application/json"
             });
-        
+            res.end(JSON.stringify(
+                { "name": "User Management API", "version": "1.0.0" }
+            ))
+        } else if (parts[1] === "") {
+            res.writeHead(200, {
+                "Content-Type": "application/json"
+            })
+
             res.end(JSON.stringify({
-                users: users
+                message: "Welcome"
+            }))
+        } else {
+            res.writeHead(404, {
+                "Content-Type": "application/json"
+            })
+
+            res.end(JSON.stringify({
+                message: "Something went wrong"
             }))
         }
-    } else if (parts[1] === 'about' && req.method === "GET") {
-        res.writeHead(200, {
-            "Content-Type": "application/json"
-        });
-        res.end(JSON.stringify(
-            { "name": "User Management API", "version": "1.0.0" }
-        ))
-    } else if (parts[1] === "" && req.method === "GET") {
-        res.writeHead(200, {
-            "Content-Type": "application/json"
-        })
+    }
 
-        res.end(JSON.stringify({
-            message: "Welcome"
-        }))
-    } else {
-        res.writeHead(404, {
-            "Content-Type": "application/json"
-        })
+    // POST METHOD
+    if (req.method === "POST") {
+        let body = "";
 
-        res.end(JSON.stringify({
-            message: "Something went wrong"
-        }))
+        if (parts[1] === "users") {
+            req.on("data", (chunk) => {
+                body += chunk;
+            });
+
+            req.on("end", () => {
+                let id = users.length + 1;
+                let newUser = JSON.parse(body);
+                newUser.id = id;
+
+                // Validate
+                const validator = typeof newUser.name === "string" && newUser.name !== null;
+                if (validator) {
+                    users.push(newUser);
+                    res.writeHead(201, {
+                        "Content-Type": "application/json"
+                    })
+    
+                    res.end(JSON.stringify({
+                        message: "Created!"
+                    }))
+                } else if (!validator) {
+                    res.writeHead(400, {
+                        "Content-Type": "application/json"
+                    })
+                    res.end(JSON.stringify({
+                        message: "Name must be a string and not null"
+                    }))
+                } else {
+                    res.writeHead(400, {
+                        "Content-Type": "application/json"
+                    })
+                    res.end(JSON.stringify({
+                        message: "Cant create user"
+                    }))
+                }
+            })
+        }
     }
 })
 
